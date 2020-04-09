b56f90d961e5bac32760369e9b607622aa84e118
Charles de Noyelle <spam@spontex.org>
2020-04-09 18:33:55 +0200

    Do not lauch FullScreen : - Requires to do -W for fullscreen or -w for windowed. - FIX required for detecting jail (default fullscreen was nice).

--- src/Version.h
+++ src/Version.h
@@ -12,6 +12,6 @@
 #include <string>
 
 // See CHANGELOG for a list of what has changed between versions
-static const std::string LinthesiaVersionString = "0.6.6";
+static const std::string LinthesiaVersionString = "0.6.6h";
 
 #endif // __LINTHESIA_VERSION_H__
